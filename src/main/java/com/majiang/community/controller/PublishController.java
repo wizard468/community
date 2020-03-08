b481bf9745325926ac35ab0fcd4208e2456a4745
wizard468 <bin_wizard@163.com>
2020-03-08 21:22:18 +0800

    add Question

--- src/main/java/com/majiang/community/controller/PublishController.java
+++ src/main/java/com/majiang/community/controller/PublishController.java
@@ -1,13 +1,34 @@
 package com.majiang.community.controller;
 
+import com.majiang.community.mapper.QuestionMapper;
+import com.majiang.community.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PublishController {
 
+    @Autowired
+    private QuestionMapper questionMapper;
+
     @GetMapping("/publish")
     public String publish(){
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title")String title,
+                            @RequestParam("description")String description,
+                            @RequestParam("tag")String tag){
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.
+        questionMapper.create(question);
         return "publish";
     }
 }
